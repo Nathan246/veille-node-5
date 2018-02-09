ffb18405777d277be9b2844c217030409659f69f
Nathan Lamothe <e1436741@cmaisonneuve.qc.ca>
2018-02-09 16:16:03 -0500

    deuxième commit

--- 01-apps.js
+++ 01-apps.js
@@ -3,22 +3,33 @@ const app = express();
 const fs = require('fs');
 app.use(express.static('public'));
 /* on associe le moteur de vue au module «ejs» */
+const bodyParser= require('body-parser')
+const MongoClient = require('mongodb').MongoClient // le pilote MongoDB
 app.set('view engine', 'ejs'); // générateur de template
 
-app.get('/', function (req, res) {
-   fs.readFile( __dirname + "/public/data/" + "membres.txt", 'utf8',(err, data) => {
-   		if (err) { 
-   			return console.error(err)
-   		}
-        console.log(data);
-        let resultat = JSON.parse('[' + data + ']');           
-  		res.render('gabarit.ejs', {adresses: resultat})
-	})
+var db // variable qui contiendra le lien sur la BD
+
+MongoClient.connect('mongodb://127.0.0.1:27017/carnet_adresse', (err, database) => {
+ if (err) return console.log(err)
+ db = database
+// lancement du serveur Express sur le port 8081
+ app.listen(8081, () => {
+ console.log('connexion à la BD et on écoute sur le port 8081')
+ })
 })
 
-var server = app.listen(8081, function () {
+app.get('/', function (req, res) {
+   let cursor = db.collection('adresse').find().toArray(function(err, resultat){
+ if (err) return console.log(err)
+ // transfert du contenu vers la vue index.ejs (renders)
+ // affiche le contenu de la BD
+ res.render('gabarit.ejs', {adresses: resultat})
+ }) 
+})
+
+/*var server = app.listen(8081, function () {
    var host = server.address().address
    var port = server.address().port
    
    console.log("Example app listening at http://%s:%s", host, port)
-})
+})*/
