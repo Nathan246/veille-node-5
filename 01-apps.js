79b059a5ba3c24043d4b673d5e71d5763e49af78
Nathan Lamothe <e1436741@cmaisonneuve.qc.ca>
2018-02-09 14:46:50 -0500

    Premier commit

---
+++ 01-apps.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+app.use(express.static('public'));
+/* on associe le moteur de vue au module «ejs» */
+app.set('view engine', 'ejs'); // générateur de template
+
+app.get('/', function (req, res) {
+   fs.readFile( __dirname + "/public/data/" + "membres.txt", 'utf8',(err, data) => {
+   		if (err) { 
+   			return console.error(err)
+   		}
+        console.log(data);
+        let resultat = JSON.parse('[' + data + ']');           
+  		res.render('gabarit.ejs', {adresses: resultat})
+	})
+})
+
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Example app listening at http://%s:%s", host, port)
+})
