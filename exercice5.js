7a291014d90f9e83d4eb2a63be3a7ef5d1e1299e
NATHAN-PC\Nathan <nathan246@live.ca>
2018-02-12 16:49:11 -0500

    Création fichier exercice5.js ex5 NathanL

---
+++ exercice5.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+app.use(express.static('public'));
+/* on associe le moteur de vue au module «ejs» */
+const bodyParser= require('body-parser')
+const MongoClient = require('mongodb').MongoClient // le pilote MongoDB
+app.set('view engine', 'ejs'); // générateur de template
+
+var db // variable qui contiendra le lien sur la BD
+
+MongoClient.connect('mongodb://127.0.0.1:27017/carnet_adresse', (err, database) => {
+ if (err) return console.log(err)
+ db = database
+// lancement du serveur Express sur le port 8081
+ app.listen(8081, () => {
+ console.log('connexion à la BD et on écoute sur le port 8081')
+ })
+})
+
+/*app.get('/', function (req, res) {
+   let cursor = db.collection('adresse').find().toArray(function(err, resultat){
+ if (err) return console.log(err)
+ // transfert du contenu vers la vue index.ejs (renders)
+ // affiche le contenu de la BD
+ res.render('gabarit.ejs', {adresses: resultat})
+ }) 
+})*/
+
+app.get('/formulaire', function (req, res) {
+   let cursor = db.collection('adresse').find().toArray(function(err, resultat){
+ if (err) return console.log(err)
+ // transfert du contenu vers la vue index.ejs (renders)
+ // affiche le contenu de la BD
+ res.render('gabarit.ejs', {adresses: resultat})
+ }) 
+})
